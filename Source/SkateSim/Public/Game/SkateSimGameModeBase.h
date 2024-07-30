9715a79f1cfe59e84f4f99a21e864cd377abca29
sha-ridi <sharipovadinag@gmail.com>
2024-07-30 12:29:34 +0400

    Add Base GameMode

---
+++ Source/SkateSim/Public/Game/SkateSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKATESIM_API ASkateSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
